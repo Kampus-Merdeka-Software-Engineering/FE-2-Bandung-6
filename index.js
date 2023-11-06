eca97a4269213e7aab2cc06409c23a956afaccf2
anisatulmarifah <anisatulmarifah16@gmail.com>
2023-11-06 22:57:05 +0700

    footer, index update

--- index.js
+++
@@ -1,9 +0,0 @@
-// Script for navbar
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-    const navLinks = document.querySelector(".nav-links");
-
-    hamburger.addEventListener("click", function () {
-    navLinks.classList.toggle("active");
-    });
-});
