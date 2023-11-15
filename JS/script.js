c7f3c0ea8721a94e5577654e3c21b9f277b3c844
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-11-15 21:15:17 +0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,72 +1,99 @@
 // Header
 function toggleMenu() {
-    var menuList = document.getElementById('menu-list');
-    var menuExtra = document.getElementById('menu-extra');
-  
-    menuList.classList.toggle('show');
-    menuExtra.classList.toggle('show');
-  }
-  /* When the user clicks on the button, 
-        toggle between hiding and showing the dropdown content */
-  function toggleDropdown(dropdownId) {
-    var dropdown = document.getElementById(dropdownId);
-  
-    // Ambil daftar semua dropdown yang ada
-    var allDropdowns = ['myDropdown', 'myDropdown2', 'myDropdown3', 'myDropdown4'];
-  
-    // Sembunyikan dropdown lainnya jika terbuka
-    allDropdowns.forEach(function (otherId) {
-      if (otherId !== dropdownId) {
-        var otherDropdown = document.getElementById(otherId);
-        if (otherDropdown.classList.contains('show')) {
-          otherDropdown.classList.remove('show');
-        }
-      }
-    });
-  
-    // Kemudian tampilkan dropdown yang sesuai
-    dropdown.classList.toggle('show');
-  }
-  function selectdropdown(answer, elementId) {
-    var selectedElement = document.getElementById(elementId);
-    selectedElement.textContent = answer;
-  }
-  function loadFooter() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        document.getElementById('foot-placeholder').innerHTML = xhr.responseText;
-      }
-    };
-    xhr.open('GET', '/html/footer.html', true);
-    xhr.send();
-  }
-  function loadHeader() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        document.getElementById('nav-placeholder').innerHTML = xhr.responseText;
-      }
-    };
-    xhr.open('GET', '/html/header.html', true);
-    xhr.send();
-  }
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName('dropdown-content');
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  var menuList = document.getElementById('menu-list');
+  var menuExtra = document.getElementById('menu-extra');
+
+  menuList.classList.toggle('show');
+  menuExtra.classList.toggle('show');
+}
+/* When the user clicks on the button, 
+      toggle between hiding and showing the dropdown content */
+function toggleDropdown(dropdownId) {
+  var dropdown = document.getElementById(dropdownId);
+
+  // Ambil daftar semua dropdown yang ada
+  var allDropdowns = ['myDropdown', 'myDropdown2', 'myDropdown3', 'myDropdown4'];
+
+  // Sembunyikan dropdown lainnya jika terbuka
+  allDropdowns.forEach(function (otherId) {
+    if (otherId !== dropdownId) {
+      var otherDropdown = document.getElementById(otherId);
+      if (otherDropdown.classList.contains('show')) {
+        otherDropdown.classList.remove('show');
       }
     }
+  });
+
+  // Kemudian tampilkan dropdown yang sesuai
+  dropdown.classList.toggle('show');
+}
+function selectdropdown(answer, elementId) {
+  var selectedElement = document.getElementById(elementId);
+  selectedElement.textContent = answer;
+}
+function loadFooter() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      document.getElementById('foot-placeholder').innerHTML = xhr.responseText;
+    }
   };
-  document.addEventListener('DOMContentLoaded', function () {
-    loadFooter();
+  xhr.open('GET', '/html/footer.html', true);
+  xhr.send();
+}
+function loadHeader() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      document.getElementById('nav-placeholder').innerHTML = xhr.responseText;
+    }
+  };
+  xhr.open('GET', '/html/header.html', true);
+  xhr.send();
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
+document.addEventListener('DOMContentLoaded', function () {
+  loadFooter();
+});
+document.addEventListener('DOMContentLoaded', function () {
+  loadHeader();
+});
+
+// phone number
+// script.js
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  var phoneInput = document.getElementById("phone");
+  var awalanDitambahkan = false;
+
+  // Tambahkan event listener untuk mendeteksi perubahan nilai pada input nomor telepon
+  phoneInput.addEventListener("input", function () {
+    if (!awalanDitambahkan) {
+      // Dapatkan nilai nomor telepon
+      var phoneNumber = phoneInput.value;
+
+      // Format nomor telepon dengan awalan +62
+      var formattedPhoneNumber = "+62" + phoneNumber;
+
+      // Atur nilai input nomor telepon
+      phoneInput.value = formattedPhoneNumber;
+
+      // Setel flag agar awalan hanya ditambahkan satu kali
+      awalanDitambahkan = true;
+    }
   });
-  document.addEventListener('DOMContentLoaded', function () {
-    loadHeader();
-  });
+});
+
