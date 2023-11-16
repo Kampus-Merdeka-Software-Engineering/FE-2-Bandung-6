7014753fa388a35e89a2857d471507703bedae82
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-11-16 19:46:26 +0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -92,43 +92,41 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 //Dropdown detail_penumpang
-let input = document.querySelector('.input-box');
-input.onclick = function () {
-  this.classList.toggle('open');
-  let list = this.nextElementSibling;
-  if (list.style.maxHeight) {
-    list.style.maxHeight = null;
-    list.style.boxShadow = null;
-  } else {
-    list.style.maxHeight = list.scrollHeight + 'px';
-    list.style.boxShadow = '0 1px 2px 0 rgba(0, 0, 0, 0.15),0 1px 3px 1px rgba(0, 0, 0, 0.1)';
-  }
-};
+let dropdowns = document.querySelectorAll('.dropdown');
 
-let rad = document.querySelectorAll('.radio');
-rad.forEach((item) => {
-  item.addEventListener('change', () => {
-    input.innerHTML = item.nextElementSibling.innerHTML;
-    input.click();
-  });
-});
-
-let label = document.querySelectorAll('label');
-function search(searchin) {
-  let searchVal = searchin.value;
-  searchVal = searchVal.toUpperCase();
-  label.forEach((item) => {
-    let checkVal = item.querySelector('.name').innerHTML;
-    checkVal = checkVal.toUpperCase();
-    if (checkVal.indexOf(searchVal) == -1) {
-      item.style.display = 'none';
+dropdowns.forEach((dropdown) => {
+  let input = dropdown.querySelector('.input-box');
+  input.onclick = function () {
+    this.classList.toggle('open');
+    let list = this.nextElementSibling;
+    if (list.style.maxHeight) {
+      list.style.maxHeight = null;
+      list.style.boxShadow = null;
     } else {
-      item.style.display = 'flex';
+      list.style.maxHeight = list.scrollHeight + 'px';
+      list.style.boxShadow = '0 1px 2px 0 rgba(0, 0, 0, 0.15),0 1px 3px 1px rgba(0, 0, 0, 0.1)';
     }
-    let list = input.nextElementSibling;
-    list.style.maxHeight = list.scrollHeight + 'px';
+  };
+
+  let rad = dropdown.querySelectorAll('.radio');
+  rad.forEach((item) => {
+    item.addEventListener('change', () => {
+      input.innerHTML = item.nextElementSibling.innerHTML;
+      input.click();
+    });
   });
-}
+
+  let label = dropdown.querySelectorAll('label');
+  function labels() {
+    label.forEach((item) => {
+      let checkVal = item.querySelector('.name').innerHTML;
+      checkVal = checkVal.toUpperCase();
+      item.style.display = 'flex';
+      let list = input.nextElementSibling;
+      list.style.maxHeight = list.scrollHeight + 'px';
+    });
+  }
+});
 
 // slide about us
 let slideIndex = 0;
