2a15bd2576aba3d2a98fc52f10bc0e2b23603381
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-08 04:10:03 +0700

    Update detail_penumpang.js

--- js/detail_penumpang.js
+++ js/detail_penumpang.js
@@ -1,19 +1,33 @@
-document
-  .getElementById("tanggalBerangkat")
-  .addEventListener("input", function () {
-    var selectedDate = new Date(this.value);
-    var currentDate = new Date();
+// document.getElementById('tanggalBerangkat').addEventListener('input', function () {
+//   var selectedDate = new Date(this.value);
+//   var currentDate = new Date();
 
-    if (selectedDate < currentDate) {
-      // alert("Tidak dapat berjalan di masa lalu.");
-      this.value = ""; // Clear the input field if an invalid date is selected
-      // add message to div with id "tanggalError"
-      document.getElementById("tanggalError").innerHTML =
-        "Tidak dapat berjalan di masa lalu.";
-    }
+//   if (selectedDate < currentDate) {
+//     // alert("Tidak dapat berjalan di masa lalu.");
+//     this.value = ''; // Clear the input field if an invalid date is selected
+//     // add message to div with id "tanggalError"
+//     document.getElementById('tanggalError').innerHTML = 'Tidak dapat berjalan di masa lalu.';
+//   }
 
-    // remove the message
-    if (selectedDate >= currentDate) {
-      document.getElementById("tanggalError").innerHTML = "";
-    }
+//   // remove the message
+//   if (selectedDate >= currentDate) {
+//     document.getElementById('tanggalError').innerHTML = '';
+//   }
+// });
+document.addEventListener('DOMContentLoaded', function () {
+  initializeFlatpickr();
+});
+function initializeFlatpickr() {
+  flatpickr('#date', {
+    altFormat: 'F j, Y',
+    dateFormat: 'Y-m-d',
+    onClose: function (selectedDates, dateStr, instance) {
+      // Update hanya jika kalender ditutup dengan pemilihan tanggal
+      if (selectedDates.length > 0) {
+        updateDropdownText(dateStr);
+        // Sembunyikan kalender setelah memilih tanggal
+        instance.close();
+      }
+    },
   });
+}
