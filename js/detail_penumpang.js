519617950f75237da9b8b98fb32f7bb9671a8999
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-08 13:47:44 +0700

    Update detail_penumpang.js

--- js/detail_penumpang.js
+++ js/detail_penumpang.js
@@ -1,19 +1,19 @@
-// document.getElementById('tanggalBerangkat').addEventListener('input', function () {
-//   var selectedDate = new Date(this.value);
-//   var currentDate = new Date();
+document.getElementById('tanggalBerangkat').addEventListener('input', function () {
+  let selectedDate = new Date(this.value);
+  let currentDate = new Date();
 
-//   if (selectedDate < currentDate) {
-//     // alert("Tidak dapat berjalan di masa lalu.");
-//     this.value = ''; // Clear the input field if an invalid date is selected
-//     // add message to div with id "tanggalError"
-//     document.getElementById('tanggalError').innerHTML = 'Tidak dapat berjalan di masa lalu.';
-//   }
+  if (selectedDate < currentDate) {
+    // alert("Tidak dapat berjalan di masa lalu.");
+    this.value = ''; // Clear the input field if an invalid date is selected
+    // add message to div with id "tanggalError"
+    document.getElementById('tanggalError').innerHTML = 'Tidak dapat berjalan di masa lalu.';
+  }
 
-//   // remove the message
-//   if (selectedDate >= currentDate) {
-//     document.getElementById('tanggalError').innerHTML = '';
-//   }
-// });
+  // remove the message
+  if (selectedDate >= currentDate) {
+    document.getElementById('tanggalError').innerHTML = '';
+  }
+});
 document.addEventListener('DOMContentLoaded', function () {
   initializeFlatpickr();
 });
