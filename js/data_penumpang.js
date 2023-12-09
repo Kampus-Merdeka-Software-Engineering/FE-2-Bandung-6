417a9f43ba26f460fa2092d9c6199420ddbc6f70
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-09 17:44:10 +0700

    Update data_penumpang.js

--- js/data_penumpang.js
+++ js/data_penumpang.js
@@ -13,7 +13,7 @@ document.getElementById('bookingForm').addEventListener('submit', function (even
   submitForm();
 });
 
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://be-2-bandung-6-production.up.railway.app';
 
 function submitForm() {
   const nama = document.getElementById('nama').value;
