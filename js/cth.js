a877ee507dd8267dc410ce0888fd0e126617c0bd
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2023-12-10 16:21:47 +0700

    Update cth.js

--- js/cth.js
+++ js/cth.js
@@ -1,6 +1,5 @@
 const API_BASE_URL = 'http://localhost:3001';
 
-
 document.addEventListener('DOMContentLoaded', () => {
   // Ambil nilai kotaAsal dari sessionStorage
   const selectedKotaAsal = sessionStorage.getItem('selectedKotaAsal');
@@ -9,27 +8,63 @@ document.addEventListener('DOMContentLoaded', () => {
   if (selectedKotaAsal) {
     // Lakukan permintaan HTTP ke server untuk mendapatkan data kota berdasarkan kota asal
     fetch(`${API_BASE_URL}/kota/${selectedKotaAsal}`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         // Tampilkan data sesuai kebutuhan
         console.log('Data Kota:', data);
-
         // Misalnya, menampilkan data pada elemen HTML
-        const container = document.getElementById('dataContainer');
-        data.forEach(kota => {
+        const container = document.getElementById('detail-kota');
+
+        data.forEach((kota) => {
           const kotaElement = document.createElement('div');
           kotaElement.innerHTML = `
-            <p>Kota Asal: ${kota.kota_asal_travel}</p>
-            <p>Alamat: ${kota.alamat_travel}</p>
-            <p>No Telepon: ${kota.no_telepon_travel}</p>
-            <img src="${kota.gambar_travel}" alt="Gambar Kota">
-            <hr>
+          <img src="${kota.gambar_travel}" alt="Gambar Kota" />
+          <h4>${kota.kota_asal_travel}</h4>
+          <div class="location">
+            <span class="material-icons-outlined"> location_on</span>
+            <span> ${kota.alamat_travel}</span>
+          </div>
+          <div class="phone">
+            <span class="material-icons-outlined"> call</span>
+            <span> ${kota.no_telepon_travel}</span>
+          </div>
           `;
           container.appendChild(kotaElement);
         });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Terjadi kesalahan saat mengambil data kota:', error);
+      });
+    fetch(`${API_BASE_URL}/layanan/${selectedKotaAsal}`)
+      .then((response) => response.json())
+      .then((kota) => {
+        // Tampilkan data layanan pada elemen HTML
+        const container = document.getElementById('detail-layanan');
+        kota.forEach((kotaData) => {
+          const layananElement = document.createElement('div');
+          layananElement.innerHTML = `
+          <div class="lokasi-awal">
+            <img src="../image/destination/location_home.svg" alt="" />
+            <span>${kotaData.kota_asal}</span>
+          </div>
+          <div class="lokasi-akhir">
+            <img src="../image/destination/location_away.svg" alt="" />
+            <span> ${kotaData.kota_tujuan}</span>
+          </div>
+          <div class="waktu">
+            <img src="../image/destination/schedule.svg" alt="" />
+            <span>${kotaData.jam_keberangkatan} WIB</span>
+          </div>
+          <div class="jumlah-kursi">
+            <img src="../image/destination/accessible.svg" alt="" />
+            <span>${kotaData.batas_tiket} Kursi</span>
+          </div>
+        `;
+          container.appendChild(layananElement);
+        });
+      })
+      .catch((error) => {
+        console.error('Terjadi kesalahan saat mengambil data layanan:', error);
       });
   } else {
     // Handle jika nilai kotaAsal tidak ada
